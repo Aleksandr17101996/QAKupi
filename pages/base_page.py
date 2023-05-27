667bf1b4d5fa18318a79aaa050ed0d47a7adcabe
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-05-27 19:33:56 +0300

    pip 8

--- pages/base_page.py
+++ pages/base_page.py
@@ -2,6 +2,7 @@ from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 class BasePage:
     def __init__(self, driver, url):
         self.driver = driver
@@ -9,4 +10,18 @@ class BasePage:
 
     def open(self):
         self.driver.get(self.url)
-    def element_is_visible
+
+    def element_is_visible(self, locators, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locators))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
