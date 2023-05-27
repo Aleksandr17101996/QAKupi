cf62011fc6c9ce2c5dc1d7f55a9dd125efd02fd5
Aleksandr17101996 <poleshuk.alex3460@yandex.ru>
2023-05-27 13:15:29 +0300

    add base_page

---
+++ pages/base_page.py
@@ -0,0 +1,12 @@
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+    def element_is_visible
